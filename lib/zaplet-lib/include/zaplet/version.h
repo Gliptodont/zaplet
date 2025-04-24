adbda24181000ef2a467e88be97d33a1e2709978
Gliptodont <onligamer99@gmail.com>
2025-04-24 16:23:51 +0300

    change version

--- lib/zaplet-lib/include/zaplet/version.h
+++ lib/zaplet-lib/include/zaplet/version.h
@@ -7,10 +7,10 @@
 
 namespace zaplet {
 
-constexpr const char* VERSION = "1.0.0";
+constexpr const char* VERSION = "1.0.2";
 constexpr int VERSION_MAJOR = 1;
 constexpr int VERSION_MINOR = 0;
-constexpr int VERSION_PATCH = 0;
+constexpr int VERSION_PATCH = 2;
 
 } // namespace zaplet
 
